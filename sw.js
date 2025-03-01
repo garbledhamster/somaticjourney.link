568260a0134e26f216ee6bbcabaf90a55321cb86
GarbledHamster <jrice@ourstuff.space>
2025-03-01 07:56:12 -0600

    overhaul

--- sw.js
+++ sw.js
@@ -1,39 +1,29 @@
-const CACHE_NAME = 'somatic-journey-cache-v1';
-const urlsToCache = [
-  'https://somaticjourney.link/', // Home page
-  'https://somaticjourney.link/index.html', // Main HTML file
-  'https://cdn.tailwindcss.com', // External CSS
-  'https://github.com/garbledhamster/somaticjourney.link/blob/main/images/favicon.png?raw=true', // Favicon
-  'https://raw.githubusercontent.com/garbledhamster/somaticjourney.link/main/cards.json', // JSON data
+const CACHE_NAME = "sj-resources-cache-v1";
+const OFFLINE_URLS = [
+  "/",
+  "/index.html",
+  "images/favicon.png",
+  "images/android-chrome-192x192.png",
+  "images/android-chrome-512x512.png",
+  "images/apple-touch-icon.png",
+  "images/favicon-32x32.png",
+  "images/favicon-16x16.png",
+  "images/favicon.ico",
+  "images/site.webmanifest"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(OFFLINE_URLS);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhiteList = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhiteList.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
     })
   );
 });
